af2a95b7f9dd1135a5607bd9f6a2bd46b97422a6
YashRevdiwala <yash.yr.2493@gmail.com>
2024-06-06 15:19:04 +0530

    data sorted

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,17 @@ import { Article } from "./_components/article"
 
 export default async function Home() {
   const fetchNews = await axios.get(process.env.NEWS_API)
+  const newsWithImages = fetchNews.data.data.filter((imageData) => {
+    return imageData.image !== null
+  })
+  const newsWithoutImages = fetchNews.data.data.filter((imageData) => {
+    return imageData.image === null
+  })
+  const newsData = newsWithImages.concat(newsWithoutImages)
+
   return (
     <div className="mx-10 grid max-w-screen-2xl grid-cols-4 gap-4">
-      {fetchNews.data.data?.map((newsData, i) => {
+      {newsData?.map((newsData, i) => {
         return <Article key={i} data={newsData} />
       })}
     </div>
