c6d438305668bf5ff85a97f95bb35fa8d458df48
Yash Revdiwala <yash.yr.2493@gmail.com>
2025-03-28 10:16:10 +0530

    TimeAgo Updated

--- app/_components/time.jsx
+++ app/_components/time.jsx
@@ -1,6 +1,16 @@
 "use client"
+
+import { useEffect, useState } from "react";
 import ReactTimeago from "react-timeago"
 
 export const TimeAgo = ({ date }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  },[]);
+
+  if (!isClient) return null;
+  
   return <ReactTimeago date={date} />
 }
