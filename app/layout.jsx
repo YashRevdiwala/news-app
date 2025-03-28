6c5b4d65739b3b9c44ce05312b0eebbdfc0e8a10
Yash Revdiwala <yash.yr.2493@gmail.com>
2025-03-28 10:05:32 +0530

    Added Vercel SpeedInsights

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google"
-import "./globals.css"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import { NavBar } from "./_components/navbar"
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,6 +18,7 @@ export default function RootLayout({ children }) {
         <div className="mx-auto max-w-7xl">
           <NavBar />
           {children}
+          <SpeedInsights />
         </div>
       </body>
     </html>
